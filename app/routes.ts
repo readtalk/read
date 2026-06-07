fbb34ae5a149c0674f9af474a722a1faa8472e9a
READTalk <redtalk.app@hotmail.com>
2026-06-07 23:32:40 +0700

    Add chat route with dynamic ID to routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,10 @@
+// app/routes.ts
 import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   layout("components/MainLayout.tsx", [
     route("/profile", "routes/profile.tsx"),
-    // route("/chat", "routes/chat.tsx"),
-    // route("/settings", "routes/settings.tsx"),
+    route("/chat/:id", "routes/chat.tsx"), // untuk mobile
   ]),
 ] satisfies RouteConfig;
