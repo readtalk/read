6c7ca583c6e7a645ba38c3d5a7eb4d3865e71af9
READTalk <redtalk.app@hotmail.com>
2026-06-07 23:50:27 +0700

    Replace ChatRoom component with chat ID display

--- app/routes/chat.tsx
+++ app/routes/chat.tsx
@@ -1,8 +1,6 @@
-// app/routes/chat.tsx
 import { useParams } from "react-router";
-import ChatRoom from "../components/ChatRoom";
 
 export default function ChatPage() {
   const { id } = useParams();
-  return <ChatRoom chatId={id || null} />;
+  return <div>Chat Room {id}</div>;
 }
