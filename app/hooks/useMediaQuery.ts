990433c60102c356cc7b075699495fb4cab44def
READTalk <redtalk.app@hotmail.com>
2026-06-07 23:25:04 +0700

    Add custom hook for media query detection

---
+++ app/hooks/useMediaQuery.ts
@@ -0,0 +1,17 @@
+// app/hooks/useMediaQuery.ts
+import { useEffect, useState } from "react";
+
+export default function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    setMatches(media.matches);
+    
+    const listener = (e: MediaQueryListEvent) => setMatches(e.matches);
+    media.addEventListener("change", listener);
+    return () => media.removeEventListener("change", listener);
+  }, [query]);
+
+  return matches;
+}
