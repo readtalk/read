4f5705d3e06dc0e4bba7ca4f1d24e36c6032141d
READTalk <redtalk.app@hotmail.com>
2026-06-07 21:09:43 +0700

    Change route for '/resendlist' to profile.tsx

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("/resendlist", "routes/resendlist.tsx"),
+  route("/resendlist", "routes/profile.tsx"),
 ] satisfies RouteConfig;
