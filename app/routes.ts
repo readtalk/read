e6f191b18d1d97768a48297b6bace4f8a19b45e9
READTalk <redtalk.app@hotmail.com>
2026-06-08 00:10:48 +0700

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,9 @@
-// app/routes.ts
 import { type RouteConfig, index, route, layout } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   layout("components/MainLayout.tsx", [
     route("/profile", "routes/profile.tsx"),
-    route("/chat/:id", "routes/chat.tsx"), // untuk mobile
+    // route("/chat/:id", "routes/chat.tsx"), // ← DIKOMENTAR DULU
   ]),
 ] satisfies RouteConfig;
