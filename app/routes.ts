34e07fd1c3f2e72a44ab111c73837411ba4a7655
READTalk <redtalk.app@hotmail.com>
2026-06-08 23:52:29 +0700

    Remove chat route from routes configuration

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("welcome/welcome.tsx"), // Ganti home.tsx langsung ke welcome
-  route("profile", "routes/profile.tsx"),
-  route("chat/:id", "routes/chat.tsx"), // <-- Aktifin ini
+  route("profile", "routes/profile.tsx"),  
 ] satisfies RouteConfig;
